a31863343399133d67e7ab0bfd18aa413dabee66
Mario Amaya <thefozcat@gmail.com>
2020-02-12 14:54:30 -0800

    added redux store

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi there!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Hi there!</Text>
+      </View>
+    </Provider>
   );
 }
 
---
+++ reducers/index.js
@@ -0,0 +1,11 @@
+// Set Initial State
+
+const initialState = {
+    reps: {}
+}
+
+function reducer(state = initialState, action) {
+    return state;
+}
+
+export default reducer;
