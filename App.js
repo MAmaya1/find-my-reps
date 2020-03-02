e4f4f2f7231ec598c28d6c11570529c109c2a13c
Mario Amaya <thefozcat@gmail.com>
2020-03-02 15:41:55 -0800

    minor cleanup

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { AsyncStorage } from 'react-native';
 
 // Import Screens
 
@@ -22,36 +21,11 @@ const store = createStore(rootReducer, applyMiddleware(thunk));
 const Stack = createStackNavigator();
 
 export default function App() {
-
-  const [storedAddress, setStoredAddress] = useState(false);
-
-  // Check for existing address in AsyncStorage
-  const checkUserAddress = async () => {
-    try {
-      const userAddress = await AsyncStorage.getItem('address');
-
-      if (userAddress) {
-        setStoredAddress(userAddress);
-        console.log('ADDRESS FROM STORAGE ====>', userAddress);
-        console.log('STATE ADDRESS ===>', storedAddress);
-      } else {
-        setStoredAddress(false);
-      }
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    checkUserAddress();
-  })
-
   return (
     <Provider store={store}>
       <NavigationContainer>
-        {/* If address exists in AsyncStorage navigate to 'My Reps', if not navigate to 'Home' */}
-        <Stack.Navigator initialRouteName={`${!storedAddress ? 'Home' : 'My Representatives'}`}>
+        {/* TODO: If address exists in AsyncStorage navigate to 'My Reps', if not navigate to 'Home' */}
+        <Stack.Navigator initialRouteName={'Home'}>
           <Stack.Screen 
             name='Home' 
             component={AddressForm}
